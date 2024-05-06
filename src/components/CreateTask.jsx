ee84232f5be5832b66d194ff9df7066683ad91d2
Nandhakumar <kumarnandha8983@gmail.com>
2024-05-06 13:19:26 +0530

    Modified to add tasks, Deadline and Priority modification

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,12 +1,14 @@
 import {React, useState} from 'react';
-import { TextField, Button, Select, InputLabel, FormControl, MenuItem, Box, Typography } from '@mui/material';
+import { TextField, Button, Select, InputLabel, FormControl, MenuItem, Box, Typography, IconButton } from '@mui/material';
 import '../App.css';
 import { Formik,Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import CloseIcon from '@mui/icons-material/Close';
+// npm install date-fns
 
-
-const CreateTask = ({isVisible}) => {
+const CreateTask = ({isVisible, onClose, onTaskSubmit}) => {
     const [formDataArray, setFormDataArray] = useState([]);
+    const today = new Date().toISOString().split('T')[0];
     const initialValues = {
         task: '',
         deadline: '',
@@ -15,11 +17,17 @@ const CreateTask = ({isVisible}) => {
         description: '',
         tags: '',
     };
+
+    const formatDate = (dateString) => {
+        const dateParts = dateString.split('-');
+        return `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+    }
     
     const onSubmit = (values, onSubmitProps) => {
+        const formattedDeadline = formatDate(values.deadline)
         const formData = {
             task: values.task,
-            deadline: values.deadline,
+            deadline: formattedDeadline,
             priority: values.priority,
             assigned: values.assigned,
             description: values.description,
@@ -28,9 +36,11 @@ const CreateTask = ({isVisible}) => {
     
         
         setFormDataArray([...formDataArray,formData]);
+        onTaskSubmit(formData);
     
         onSubmitProps.resetForm();
         console.log(formDataArray);
+        onClose();
     };
     
     const Validation = Yup.object({
@@ -41,66 +51,69 @@ const CreateTask = ({isVisible}) => {
         description : Yup.string().required('* Description is required'),
         tags : Yup.string().required('* Tags are required'),
     });
+
+    const [focused,setFocused] = useState(false);
     
-  const [focused,setFocused] = useState(false);
-
   return (
-    <Box className={`bg-gray-200 overflow-hidden fixed top-[2vw] left-[35vw] w-[35vw] shadow-xl shadow-white p-5 rounded-md flex-col justify-center items-center newTask_form ${isVisible ? 'flex' : 'hidden'}`}>
+    <Box className={`bg-gray-200 overflow-hidden fixed top-[2vw] left-[35vw] w-[35vw] shadow-xl shadow-white p-5 rounded-md newTask_form ${isVisible ? 'inline' : 'hidden'}`}>
+        <IconButton sx={{float: 'right'}} onClick={onClose}><CloseIcon sx={{color: 'red', fontSize: '2rem'}}/></IconButton>
 
-        <Typography variant='h4' sx={{margin: '1vw 0'}}>Add New Task</Typography>
+        <Typography variant='h4' sx={{margin: '0.1vw 0 1vw', textAlign: 'center'}}>Add New Task</Typography>
 
-        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={Validation}>
-           { (formik) => (
+        <div className='flex justify-center items-center'>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={Validation}>
+            { (formik) => (
 
-                <Form className='flex flex-col justify-center items-center w-[30vw]'>
+                    <Form className='flex flex-col justify-center items-center w-[30vw]'>
 
-                    <FormControl fullWidth>
-                        <Field as={TextField} label={<span>Task<span className='text-red-700'>*</span></span>} fullWidth name='task'/>
-                        <ErrorMessage name='task'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
-                    
-                    <FormControl sx={{margin: '2vw 0'}} fullWidth>
-                        <Field as={TextField}label={ focused ? <span>Deadline<span className='text-red-700'>*</span></span> : null} type='date' fullWidth onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} name='deadline'/>
-                        <ErrorMessage name='deadline'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
-                    
-                    <FormControl fullWidth>
-                        <InputLabel id='priority_types'><span>Priority<span className='text-red-700'>*</span></span></InputLabel>
-                        <Field as={Select} name='priority' labelId='priority_types' label={<span>Priority<span className='text-red-700'>*</span></span>}>
-                            <MenuItem value='Low'>Low</MenuItem>
-                            <MenuItem value='Medium'>Medium</MenuItem>
-                            <MenuItem value='High'>High</MenuItem>
-                        </Field>
-                        <ErrorMessage name='priority'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
+                        <FormControl fullWidth>
+                            <Field as={TextField} label={<span>Task<span className='text-red-700'>*</span></span>} fullWidth name='task'/>
+                            <ErrorMessage name='task'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
+                        
+                        <FormControl sx={{margin: '2vw 0'}} fullWidth>
+                            <Field as={TextField} label={ focused ? <span>Deadline<span className='text-red-700'>*</span></span> : null} type='date' InputProps={{inputProps: {min:today,},}} fullWidth onFocus={() => setFocused(true)} onBlur={() => setFocused(false)} name='deadline'/>
+                            <ErrorMessage name='deadline'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
+                        
+                        <FormControl fullWidth>
+                            <InputLabel id='priority_types'><span>Priority<span className='text-red-700'>*</span></span></InputLabel>
+                            <Field as={Select} name='priority' labelId='priority_types' label={<span>Priority<span className='text-red-700'>*</span></span>}>
+                                <MenuItem value='Low'>Low</MenuItem>
+                                <MenuItem value='Medium'>Medium</MenuItem>
+                                <MenuItem value='High'>High</MenuItem>
+                            </Field>
+                            <ErrorMessage name='priority'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
 
-                    <FormControl fullWidth sx={{margin: '2vw 0'}}>
-                        <InputLabel id='assigned'><span>Assigned to<span className='text-red-700'>*</span></span></InputLabel>
-                        <Field as={Select} name='assigned' labelId='assigned' label={<span>Assigned to<span className='text-red-700'>*</span></span>}>
-                            <MenuItem value='sakthikarthick'>Sakthikarthick</MenuItem>
-                            <MenuItem value='kishorekumar'>Kishorekumar</MenuItem>
-                            <MenuItem value='aswin'>Aswin</MenuItem>
-                            <MenuItem value='saran'>Saran</MenuItem>
-                        </Field>
-                        <ErrorMessage name='assigned'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
+                        <FormControl fullWidth sx={{margin: '2vw 0'}}>
+                            <InputLabel id='assigned'><span>Assigned to<span className='text-red-700'>*</span></span></InputLabel>
+                            <Field as={Select} name='assigned' labelId='assigned' label={<span>Assigned to<span className='text-red-700'>*</span></span>}>
+                                <MenuItem value='sakthikarthick'>Sakthikarthick</MenuItem>
+                                <MenuItem value='kishorekumar'>Kishorekumar</MenuItem>
+                                <MenuItem value='aswin'>Aswin</MenuItem>
+                                <MenuItem value='saran'>Saran</MenuItem>
+                            </Field>
+                            <ErrorMessage name='assigned'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
 
-                    <FormControl fullWidth>
-                        <Field as={TextField} name='description' label={<span>Description<span className='text-red-700'>*</span></span>} multiline rows={4} fullWidth/>
-                        <ErrorMessage name='description'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
+                        <FormControl fullWidth>
+                            <Field as={TextField} name='description' label={<span>Description<span className='text-red-700'>*</span></span>} multiline rows={4} fullWidth/>
+                            <ErrorMessage name='description'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
 
-                    <FormControl sx={{margin: '2vw 0'}} fullWidth>
-                        <Field as={TextField} name='tags' label={<span>Tags<span className='text-red-700'>*</span></span>} fullWidth/>
-                        <ErrorMessage name='tags'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
-                    </FormControl>
+                        <FormControl sx={{margin: '2vw 0'}} fullWidth>
+                            <Field as={TextField} name='tags' label={<span>Tags<span className='text-red-700'>*</span></span>} fullWidth/>
+                            <ErrorMessage name='tags'>{ (errormessage) => <div className='text-red-500'>{errormessage}</div>}</ErrorMessage>
+                        </FormControl>
 
-                    <Button variant='contained' type='submit'>Create</Button>
-                </Form>
+                        <Button variant='contained' type='submit'>Create</Button>
+                    </Form>
 
-            )}
+                )}
 
-        </Formik>
+            </Formik>
+        </div>
 
     </Box>
   )
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,8 +3,10 @@ import AddIcon from '@mui/icons-material/Add';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { IconButton, Avatar } from '@mui/material';
 import CreateTask from './CreateTask';
+import '../index.css';
 
 const TaskList = () => {
+    const [taskData, setTaskData] = useState([]);
     const [isVisible, setIsVisible] = useState(false);
 
     const handleTaskClick = () => {
@@ -12,7 +14,7 @@ const TaskList = () => {
     }
   
   return (
-    <div className='grid grid-cols-3 gap-10 mx-[25vw] mt-[2vw]'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-[25vw] mt-[2vw]'>
         
         <div>
             <div className='flex justify-between overflow-hidden items-center h-[6vh] bg-white shadow-lg shadow-gray-300 px-4 py-2 border rounded-md'>
@@ -20,26 +22,30 @@ const TaskList = () => {
                     <h1 className='text-black text-xl mr-2'>To-do</h1>
                     <Avatar sx={{width:'1.3rem', height: '1.3rem', color: 'black'}}>5</Avatar>
                 </div>
-                <IconButton onClick={() => <CreateTask isVisible={handleTaskClick}/>}><AddIcon className='mr-2 text-black'/></IconButton>
+                <IconButton onClick={handleTaskClick}><AddIcon className='mr-2 text-black'/></IconButton>
+                {isVisible && <CreateTask isVisible={isVisible} onClose={() => setIsVisible(false)} onTaskSubmit={(data) => {setTaskData([data,...taskData])}}/>}
             </div>
 
-            <div className='mt-[2vw] bg-white shadow-lg shadow-gray-300 rounded-lg p-3'>
+            {taskData && taskData.map((task, index) => (
+                <div key={index} className='mt-[2vw] bg-white shadow-lg shadow-gray-300 rounded-lg p-3 overflow-hidden cursor-pointer'>
                
-                <div className='flex justify-between items-center'>
-                    <h5>Documentation</h5>
-                    <IconButton><MoreVertIcon className='text-black'/></IconButton>
-                </div>
-                
-                <p className='text-gray-500'>Deadline : 4/4/24</p>
-                <p>Saran</p>
-                <hr className='border border-gray-300 my-2'/>
+                    <div className='flex justify-between items-center'>
+                        <h5>{task.task}</h5>
+                        <IconButton><MoreVertIcon className='text-black'/></IconButton>
+                    </div>
+                    
+                    <p className='text-gray-500'>Deadline : {task.deadline}</p>
+                    <p>{task.assigned}</p>
+                    <hr className='border border-gray-300 my-2'/>
 
-                <div className='flex justify-between items-center'>
-                    <span className='border border-gray-400 rounded-full py-0.3 px-2'>#Report</span>
-                    <p className='bg-yellow-500 rounded-full text-white py-0.3 px-2'>medium</p>
-                </div>
+                    <div className='flex justify-between items-center'>
+                        <span className='border border-gray-400 rounded-full py-0.3 px-2'>#{task.tags}</span>
+                        <p className={`${task.priority.toLowerCase()} rounded-full text-white py-0.3 px-2`}>{task.priority}</p>
+                    </div>
 
-            </div>
+                </div>
+            ))}
+
 
         </div>
 
